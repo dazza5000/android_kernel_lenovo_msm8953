HighwayStar/android_kernel_lenovo_tb8704

Import Lenovo Tab 4 8 plus kernel
* sources from lenovo_tab4_8plus_osc_tab8704f_8704x.tar.bz2 imported
over LA.UM.5.6.r1-02900-89xx.0 caf tag, found by best_kernel script

drivers/leds/leds-qpnp.c
@@ -3546,6 +3546,9 @@ static int qpnp_get_config_pwm(struct pwm_config_data *pwm_cfg,
 		pwm_cfg->lut_params.idx_len =
 			pwm_cfg->duty_cycles->num_duty_pcts;
 	}
+#if defined(CONFIG_KERNEL_CUSTOM_P3590) || defined(CONFIG_KERNEL_CUSTOM_P3592)
+	pwm_disable(pwm_cfg->pwm_dev);
+#endif
 	return 0;
 
 bad_lpg_params:

drivers/video/msm/mdss/mdss_fb.c
@@ -1713,6 +1713,9 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 		current_bl = mfd->bl_level;
 		mfd->allow_bl_update = true;
 		mdss_fb_set_backlight(mfd, 0);
+#if defined(CONFIG_KERNEL_CUSTOM_P3590)  || defined(CONFIG_KERNEL_CUSTOM_P3592)
+		mdelay(50);
+#endif
 		mfd->allow_bl_update = false;
 		mfd->unset_bl_level = current_bl;
 		mutex_unlock(&mfd->bl_lock);

dazza5000/android_kernel_lenovo_msm8953

Change-Id: Ia1d80d4dd9e410c7e02c3728d79e0773190ec501

drivers/leds/leds-qpnp.c
@@ -3691,6 +3691,9 @@ static int qpnp_get_config_pwm(struct pwm_config_data *pwm_cfg,
 		pwm_cfg->lut_params.idx_len =
 			pwm_cfg->duty_cycles->num_duty_pcts;
 	}
+#if defined(CONFIG_MACH_LENOVO_TB8704) || defined(CONFIG_MACH_LENOVO_TB8804)
+	pwm_disable(pwm_cfg->pwm_dev);
+#endif
 	return 0;
 
 bad_lpg_params:

drivers/video/msm/mdss/mdss_fb.c
@@ -2084,6 +2084,9 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 		current_bl = mfd->bl_level;
 		mfd->allow_bl_update = true;
 		mdss_fb_set_backlight(mfd, 0);
+#if defined(CONFIG_MACH_LENOVO_TB8704)  || defined(CONFIG_MACH_LENOVO_TB8804)
+		mdelay(50);
+#endif
 		mfd->allow_bl_update = false;
 		mfd->unset_bl_level = current_bl;
 		mutex_unlock(&mfd->bl_lock);